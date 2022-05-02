7507be1a7f4cc7abcc71cf07bcd43a1559bcf911
Molly Obermeier <obermeier.mol@gmail.com>
2022-05-02 15:06:42 -0600

    homework

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
 import random
 import json
+from smash import Battle, Character
+
 characters = []
+
+player_character = {}
+opponent = {}
 
 with open('characters.json') as json_file:
     characters = json.load(json_file)
+
+
+def find_character(name):
+    for character in characters:
+        if name == character["name"]:
+            return character
+    return None
+
+
+def game():
+    character = input(
+        'Type a chracter name, if you do not select a name a random one will be found ')
+    if not character:
+        character = random.choice(characters)["name"]
+        # character = find_character(selected_character)
+    print("CHARACTER:" + character)
+    player_character = find_character(character)
+
+    opponent = random.choice(characters)
+
+    user = Character(player_character["name"], 50, player_character["attacks"])
+    computer = Character(opponent["name"], 50, opponent["attacks"])
+
+    # print(user.moves)
+    new_game = Battle(user, computer)
+    new_game.fight()
+
+
+game()
--- smash.py
+++ smash.py
@@ -1,1 +1,33 @@
 import random
+
+
+class Character:
+    def __init__(self, name, health, moves):
+        self.name = name
+        self.health = health
+        self.moves = moves
+
+    def bad_health(self, damage):
+        self.health -= damage
+
+    def random_move(self):
+        return random.choice(self.moves)
+
+
+class Battle:
+    def __init__(self, character_one, character_two):
+        self.character_one = character_one
+        self.character_two = character_two
+
+    def fight(self):
+        # print(self.character_two.random()["damage"])
+        while self.character_one.health > 0 and self.character_two.health > 0:
+            self.character_one.bad_health(
+                self.character_two.random_move()['damage'])
+            # print(self.character_one.health)
+            self.character_two.bad_health(
+                self.character_one.random_move()['damage'])
+            # print(self.character_two.health)
+
+        print("The winner is: ", self.character_one.name if self.character_one.health >
+              0 else self.character_two.name)
