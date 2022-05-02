9b7049ca64ab4b06575d2cf77c4c64cb3d2c5b04
Chinwendu <chinwenduukoha@gmail.com>
2022-05-02 17:33:41 -0400

    complete

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
 import random
 import json
+from smash import Character, Battle
+
 characters = []
+
+player = {}
+enemy = {}
 
 with open('characters.json') as json_file:
     characters = json.load(json_file)
+
+
+def find_character(name):
+    for character in characters:
+        if name == character["name"]:
+            return character
+    return None
+
+
+def game():
+    selected = input('Choose your Character: ')
+    if not selected:
+        selected = random.choice(characters)
+    print('You have chosen ' + selected)
+    player = find_character(selected)
+    print(player)
+
+    enemy = random.choice(characters)
+
+    current_player = Character(player["name"], 60, player["attacks"])
+    computer = Character(enemy["name"], 60, enemy["attacks"])
+
+    new_game = Battle(current_player, computer)
+    new_game.fight()
+
+
+game()
