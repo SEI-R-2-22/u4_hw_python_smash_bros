37d77021f8b796f99ad873616ceb2e95b43fbc4c
canhvo16 <canhvo16@gmail.com>
2022-05-02 16:48:38 -0500

    game created

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 import random
 import json
+from smash import Character, Battle
 characters = []
 
 with open('characters.json') as json_file:
     characters = json.load(json_file)
+
+
+def game():
+    print("Choose you're character!")
+    champion = input()
+
+    person = {}
+    computer = {}
+
+    for character in characters:
+        if character["name"] == champion or character["name"].lower() == champion:
+            person = character
+
+    computer = random.choice(characters)
+
+    player = Character(person["name"], person["attacks"])
+    comp = Character(computer["name"], computer["attacks"])
+
+    print("You have chosen " + person["name"] +
+          " and you will be fighting " + computer["name"])
+
+    battle = Battle(player, comp)
+    battle.fight()
+
+
+game()
--- smash.py
+++ smash.py
@@ -1,1 +1,34 @@
 import random
+import json
+
+file = open('characters.json')
+characters = json.load(file)
+
+
+class Character:
+    def __init__(self, name, attacks):
+        self.name = name
+        self.health = 100
+        self.attacks = attacks
+
+    def attack(self, enemy):
+        move = random.choice(self.attacks)
+        enemy.health = enemy.health - move["damage"]
+        print(self.name + " has hit " +
+              enemy.name + " with " + move["name"])
+        print(enemy.name + " health is now " + str(enemy.health))
+
+
+class Battle:
+    def __init__(self, player, computer):
+        self.player = player
+        self.computer = computer
+
+    def fight(self):
+        while (self.player.health > 0 and self.computer.health > 0):
+            self.player.attack(self.computer)
+            self.computer.attack(self.player)
+        if self.player.health <= 0:
+            print(self.player.name + " wins!")
+        elif self.computer.health <= 0:
+            print(self.computer.name + " wins!")
