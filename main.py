4b93f3882972988732ec56f484645e2ed83a0697
Dan Ventura <danice.ventura@gmail.com>
2022-05-02 15:59:37 -0600

    progress

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 import random
 import json
+from smash import Character, Battle
+
 characters = []
+char_names = []
+player_char = {}
+nme_char = {}
 
 with open('characters.json') as json_file:
     characters = json.load(json_file)
+
+
+def game():
+    for character in characters:
+        print(character["name"])
+        char_names.append(character["name"])
+    chooseChar = input('Choose your character: ')
+    if chooseChar in char_names:
+        for character in characters:
+            if chooseChar == character["name"]:
+                player_char = character
+                print('You choose: ' + str(character["name"]))
+            else:
+                None
+    else:
+        player_char = random.choice(characters)
+        print('Invalid choice, ' +
+              str(player_char["name"]) + ' has been chosen for you.')
+    print('Player Char', player_char)
+    nme_char = random.choice(characters)
+    print('Your opponent is: ' + nme_char["name"])
+    print('NME Char', nme_char)
+
+    player = Character(player_char["name"], 50, player_char["attacks"])
+    computer = Character(nme_char["name"], 50, nme_char["attacks"])
+
+    new_game = Battle(player, computer)
+    # new_game.fight()
+
+
+game()
--- smash.py
+++ smash.py
@@ -1,1 +1,54 @@
 import random
+import json
+characters = []
+
+with open('characters.json') as json_file:
+    characters = json.load(json_file)
+
+
+class Character:
+    def __init__(self, name, health, moves):
+        self.name = name
+        self.health = health
+        self.moves = []
+
+    def new_attack(self, attack):
+        self.moves.append(attack)
+
+    def dec_health(self, dec):
+        self.health -= dec
+
+    def random_atk(self):
+        return (random.choice(self.moves))
+
+
+# rando = Character('Dan', 10, [])
+# rando.new_attack('punch')
+# rando.new_attack('kick')
+# rando.dec_health(5)
+# rando.random_atk()
+
+# class Char1(Character):
+#     def __init__(self, name, health, moves):
+#         super().init(name, health, moves)
+
+
+# class Char2(Character):
+#     def __init__(self, name, health, moves):
+#         super().init(name, health, moves)
+
+
+class Battle:
+    def __init__(self, char1, char2):
+        self.char1 = char1
+        self.char2 = char2
+
+    def fight(self):
+        while self.char1.health > 0 and self.char2.health > 0:
+            self.char1.dec_health(self.char2.random_atk()["damage"])
+            print(self.char2.name + ' attacks! ' + self.char1.name +
+                  ' is now at ' + self.char1.health + ' health.')
+            self.char2.dec_health(self.char1.random_atk()["damage"])
+            print(self.char1.name + ' attacks! ' + self.char2.name +
+                  ' is now at ' + self.char2.health + ' health.')
+        print('Winner is ' + self.char1.name if self.char1.health else self.char2.name)
