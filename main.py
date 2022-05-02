986eb9a2397acfca5cc518f06d472398646c04fc
Lawrence <loreganlloyd@gmail.com>
2022-05-02 19:42:20 -0400

    completed

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
+from smash import Character, Battle
+import sys
 import random
 import json
-characters = []
+sys.path.append(".")
+
 
 with open('characters.json') as json_file:
     characters = json.load(json_file)
+    moves_by_character_dict = {}
+    for character in characters:
+        moves_by_character_dict.update(
+            {character["name"]: character["attacks"]})
+
+    print('Welcome to Smash Bros! Here are the available characters: ')
+    for character in characters:
+        print(character["name"])
+
+    player_character_name = input('Choose your character: ')
+
+    print(f'You have chosen: {player_character_name}')
+    # print("Their moves are:" + str(moves_by_character_dict[player_character_name]))
+
+    computer_character = random.choice(characters)
+    computer_character_name = computer_character["name"]
+
+    print("Computer has chosen: " + computer_character_name)
+
+    character1 = Character(player_character_name, 100,
+                           moves_by_character_dict[player_character_name], 0)
+    character2 = Character(computer_character_name, 100,
+                           moves_by_character_dict[computer_character_name], 0)
+
+    battle = Battle(character1, character2, False)
+
+    battle.start_battle()
+
+
+# Joey = Animal('Joey', 'Terrier', [])
--- smash.py
+++ smash.py
@@ -1,1 +1,45 @@
 import random
+
+
+class Character:
+    def __init__(self, name, health, moves, damage_dealt):
+        self.name = name
+        self.health = 100
+        self.moves = moves
+        damage_dealt = 0
+
+    def attack(self):
+        random_move = random.choice(self.moves)
+        print(self.name + " uses " + random_move["name"])
+        self.damage_dealt = random_move["damage"]
+        print("It does " + str(self.damage_dealt) + " damage.")
+
+    def take_damage(self, damage):
+        self.health = self.health - damage
+        print(self.name + "'s health is now " + str(self.health) + "!")
+
+
+class Battle:
+    def __init__(self, character1, character2, winner_declared):
+        self.character1 = character1
+        self.character2 = character2
+        self.winner_declared = False
+
+    def start_battle(self):
+
+        while self.winner_declared == False:
+            self.character1.attack()
+            self.character2.take_damage(self.character1.damage_dealt)
+
+            if self.character2.health <= 0:
+                print(self.character1.name + " is the winner!")
+                self.winner_declared = True
+                return
+            else:
+                self.character2.attack()
+                self.character1.take_damage(self.character2.damage_dealt)
+
+            if self.character2.health <= 0:
+                print(self.character1.name + " is the winner!")
+                self.winner_declared = True
+                return
