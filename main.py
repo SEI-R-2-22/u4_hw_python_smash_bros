a3158562c539a0d21db9141e8a689e44a6ca41c2
TakKwan <waslf3651@gmail.com>
2022-05-02 15:54:21 -0400

    done

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 import random
 import json
+import smash
 characters = []
 
 with open('characters.json') as json_file:
     characters = json.load(json_file)
+
+def game():
+    #choosing characters
+    for character in characters:
+        print(character["name"])
+    print("Please select a character")
+    selected_character = input()
+    player_character = [character for character in characters if selected_character in character["name"]]
+    if player_character == []:
+        print("A random character has been chosen for you")
+        player_character = random.choice(characters)
+    else :
+        player_character = player_character.pop()
+    player = smash.Character(player_character["name"], player_character["attacks"])
+    print("Your character is: " + player.name)
+
+    opponent_character = random.choice(characters)
+    opponent = smash.Character(opponent_character["name"], opponent_character["attacks"])
+    print("Your opponent is: " + opponent.name)
+
+    #battle
+    duel = smash.Battle(player, opponent)
+    while player.health > 0 and opponent.health > 0:
+        duel.run()
+    if player.health >= opponent.health:
+        print("player wins")
+    else :
+        print("opponent wins")
+
+game()
--- smash.py
+++ smash.py
@@ -1,1 +1,27 @@
 import random
+
+class Battle:
+  def __init__(self, character1, character2):
+    self.character1 = character1
+    self.character2 = character2
+
+  def run(self):
+    self.character1.take_hit(self.character2.use_random_skill())
+    self.character2.take_hit(self.character1.use_random_skill())
+    print("\n")
+
+
+class Character:
+  def __init__(self, name, attacks):
+    self.name = name
+    self.health = 100
+    self.attacks = attacks
+      
+  def take_hit(self, skill):
+    self.health -= skill["damage"]
+    print(self.name + " took " + str(skill["damage"]) + " damge and has remaining health: " + str(self.health))
+
+  def use_random_skill(self):
+    attack = random.choice(self.attacks)
+    print(self.name + " uses " + attack["name"])
+    return attack
