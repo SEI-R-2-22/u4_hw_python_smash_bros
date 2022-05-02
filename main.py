3a1bfe070d4f4b56148184569e836cab595c02cc
Nick Lopez <nicklopez5454@yahoo.com>
2022-05-02 18:04:15 -0500

    complete

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
+from smash import Battle, Character
 import random
 import json
 characters = []
 
 with open('characters.json') as json_file:
     characters = json.load(json_file)
+
+
+def find_character(name):
+    for character in characters:
+        if name == character["name"]:
+            return character
+    return random.choice(characters)
+
+
+def game():
+    character = input("Select your character: ")
+    if not character:
+        player_character = random.choice(characters)
+
+    player_character = find_character(character)
+    print("Your character is " + player_character)
+
+    enemy_character = random.choice(characters)
+
+    player = Character(
+        player_character["name"], player_character["attacks"], 100)
+    enemy = Character(enemy_character["name"], enemy_character["attacks"], 100)
+    fight = Battle(player, enemy)
+    fight.battle_start()
+
+
+game()
--- smash.py
+++ smash.py
@@ -1,1 +1,33 @@
 import random
+
+
+class Character:
+    def __init__(self, name, attacks, health):
+        self.name = name
+        self.attacks = attacks
+        self.health = health
+
+    def attack(self, enemy):
+        enemy.health -= random.choice(self.attacks)["damage"]
+        print(self.name + " goes for an attack! They're using " +
+              random.choice(self.attacks)["name"])
+        print(enemy.name + " took " + str(random.choice(self.attacks)
+              ["damage"]) + " points of damage!")
+
+
+class Battle:
+    def __init__(self, char1, char2):
+        self.char1 = char1
+        self.char2 = char2
+
+    def battle_start(self):
+        print("Player 1 is " + self.char1.name,
+              "Player 2 is " + self.char2.name)
+        while self.char1.health > 0 and self.char2.health > 0:
+            self.char1.attack(self.char2)
+            self.char2.attack(self.char1)
+
+            if self.char1.health < 0:
+                print(self.char2.name + " Wins!")
+            elif self.char2.health < 0:
+                print(self.char1.name + " Wins!")
