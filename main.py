97d6aff7b4be8a614bbb078be661355af1464742
Jenna Leopold <jleopold424@gmail.com>
2022-05-02 17:22:35 -0400

    completed hw

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 import random
 import json
+from smash import Battle, Character
 characters = []
 
 with open('characters.json') as json_file:
     characters = json.load(json_file)
+
+
+def find_character(character_name):
+    for character in characters:
+        if character_name.lower() == character["name"].lower():
+            return character
+    return None
+
+
+def game():
+    character_name = input("Type a character's name: ")
+    character = find_character(character_name)
+    player = Character(character["name"], 50, character["attacks"])
+    if not character_name or character is None:
+        character = random.choice(characters)
+
+    opponent = random.choice(characters)
+    enemy = Character(opponent["name"], 50, opponent["attacks"])
+
+    new_game = Battle(player, enemy)
+    new_game.battling()
+
+
+game()
--- smash.py
+++ smash.py
@@ -1,1 +1,35 @@
 import random
+
+
+class Character:
+    def __init__(self, name, health, moves):
+        self.name = name
+        self.health = health
+        self.moves = moves
+
+    def decrement_health(self, damage):
+        self.health -= damage
+
+    def random_move(self):
+        return random.choice(self.moves)
+
+
+class Battle:
+    def __init__(self, character_one, character_two):
+        self.character_one = character_one
+        self.character_two = character_two
+
+    def battling(self):
+        while self.character_one.health > 0 and self.character_two.health > 0:
+            attack1 = self.character_one.random_move()
+            self.character_two.decrement_health(attack1["damage"])
+            print("{} is using {}, and damages {} {} health, remaining {} hp".format(
+                self.character_one.name, attack1["name"], self.character_two.name, attack1["damage"], self.character_two.health))
+            if self.character_two.health <= 0:
+                return print("The winner is: {}".format(self.character_one.name))
+            attack2 = self.character_two.random_move()
+            self.character_one.decrement_health(attack2["damage"])
+            print("{} is using {}, and damages {} {} health,  remaining {} hp".format(
+                self.character_two.name, attack2["name"], self.character_one.name, attack1["damage"], self.character_one.health))
+            if self.character_one.health <= 0:
+                return print("The winner is: {}".format(self.character_two.name))
