ed55aac8bfbd4f11fcef470661386f9f9810af90
Job85 <julianjernigan@ymail.com>
2022-05-03 01:01:24 -0400

    printed list of fighter names

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import random
 import json
+from unicodedata import name
 from smash import Battle, Character
 
 characters = []
 CPU = None
 PC = None
 
+
 with open('characters.json') as json_file:
     characters = json.load(json_file)
+
+
+for character in characters:
+    print(character["name"])
+
+character_choice = input()
