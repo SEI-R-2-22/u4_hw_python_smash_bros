28e356d58162c9d5d91ed716e6f54785a9779d2f
Huan <huan.zeng1@gmail.com>
2022-05-02 22:36:37 -0400

    completed

--- main.py
+++ main.py
@@ -1,6 +1,56 @@
 import random
 import json
+from smash import Character, Battle
+from threading import Timer
+
 characters = []
+
+def ask_for_input(user_input):
+    while not (int(user_input) >= 0 and int(user_input) <= 58):
+        try:
+        
+            user_input = int(input('Select a player from above: '))
+        except ValueError:
+            print('Integer only')
+    return user_input if user_input != 58 else random.randrange(0, 57)
+
+def random_computer():
+    random_computer = random.randrange(0, 57)
+    computer = Character(characters[random_computer].get("name"), 100, characters[random_computer].get("attacks"))
+    return computer
+
+def restart_game():
+    while True:
+        res = input('Do you want to play again? y/n ')
+
+        if res.upper() == 'Y':
+            main()
+        elif res.upper() == 'N':
+            break
 
 with open('characters.json') as json_file:
     characters = json.load(json_file)
+
+def main():
+    player = ''
+    computer = ''
+    user_input = -1
+    
+    for char in enumerate(characters):
+        print((str(char[0]) + " " + char[1].get('name')))
+    print("58 Random character")
+    print('You have 10 second to choose a character')
+
+    user_input = ask_for_input(user_input)
+    
+    player = Character(characters[user_input].get("name"), 100, characters[user_input].get("attacks"))
+
+    computer = random_computer()
+
+    battle = Battle(player, computer)
+
+    battle.start_battle()
+
+    restart_game()
+
+main()
--- smash.py
+++ smash.py
@@ -1,1 +1,53 @@
 import random
+import json
+import time
+
+class Battle:
+  def __init__(self, Character1, Character2):
+      self.Character1 = Character1
+      self.Character2 = Character2
+
+  def start_battle(self):
+    random_player_attack = [self.Character1, self.Character2]
+    attacking_player = ''
+    defending_player = ''
+    attack_move = ''
+
+    print(f"***THE BATTLE BTW **{self.Character1.name}** AND **{self.Character2.name}** WILL BEGIN IN ")
+    for i in range(1, 4):
+      print(4 - i)
+      time.sleep(1)
+    
+    while True:
+      time.sleep(1)
+      attacking_player = random.choice(random_player_attack)
+      defending_player = self.Character1 if attacking_player != self.Character1 else self.Character2
+      attack_move = attacking_player.random_attack()
+
+      print(f"{attacking_player.name} attacks with {attack_move.get('name')}, {defending_player.name} took {attack_move.get('damage')} damage.")
+
+      if defending_player == self.Character1:
+        self.Character1.decrement_health(attack_move.get('damage'))
+        print(f"{self.Character1.name}'s health is {self.Character1.health}")
+        if self.Character1.health <= 0:
+          print(f"{self.Character2.name} Wins!!!")
+          break
+      else:
+        self.Character2.decrement_health(attack_move.get('damage'))
+        print(f"{self.Character2.name}'s health is {self.Character2.health}")
+        if self.Character2.health <= 0:
+          print(f"{self.Character1.name} Wins!!!")
+          break
+
+
+class Character:
+  def __init__(self, name, health, attacks):
+      self.name = name
+      self.health = health
+      self.attacks = attacks
+
+  def decrement_health(self, decrement):
+    self.health -= decrement
+  
+  def random_attack(self):
+    return random.choice(self.attacks)
