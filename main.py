1f4b61d00821884d0a5c87b7c529bd17aa23301f
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-05-03 08:40:03 -0400

    attempted homework

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
 import random
 import json
 characters = []
+from smash import Character
+from smash import Battle
 
 with open('characters.json') as json_file:
     characters = json.load(json_file)
+
+ ############ I AM SO SORRY! I REALLY STRUGGLE WITH THIS ONE :-( ##########
+
+
+def game():
+   game_character = input("Please select your character")
+ 
+   character_info = [x for x in characters if x['name'] == game_character]
+   player_character = Character(game_character, 100, character_info[0]['attacks'])
+   if not player_character or character_info is None:
+           game_character = random.choice(characters)
+   else:
+       print("The player has selected: " + player_character ) 
+
+
+for character in characters:
+    comp_index = random.randint(0, len(characters)-1)
+computer = Character(characters[comp_index]["name"], 100, characters[comp_index]["attacks"])
+print("Computer has selected: " + computer["name"])
+   
+start = input("Press s to start the battle!")
+begin = str(start)
+if begin == 's':
+    Battle.fight(player_character, computer)
+    Character.decrement_health()
+    Character.select_attack()
+
+game() 
+
+play_again = input("Would you like to play again y/n?")
+play = str(play_again)  
+if play == 'y': 
+    game()   
+
+
+
+
+
+
+
--- smash.py
+++ smash.py
@@ -1,1 +1,38 @@
 import random
+import json
+characters = []
+
+with open('characters.json') as json_file:
+    characters = json.load(json_file)
+    #print(characters)
+    
+class Character:
+    def __init__(self, name, health, moves):
+        self.name = name
+        self.health = health
+        self.moves = moves
+        
+        
+        def decremet_health(self):
+             self.health = int(self.health - 1)
+        
+        def select_attack(self):
+            index =random.randint(0, len(self.moves))
+            return self.moves[index]
+
+
+class Battle:
+    def __init__(self, char1, char2):
+        self.char1 = char1
+        self.char2 = char2
+        
+    def fight(self):
+        if self.char2.health <= 0:
+            print("The winner is: {}".format(self.char1.name))
+        elif self.char1.health <= 0:
+            print("The winner is: {}".format(self.char2.name))
+        else:
+            print("It's a draw!")
+        
+        
+     
