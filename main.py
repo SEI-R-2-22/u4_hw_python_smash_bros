f9aa767e798bbe291e90f1015cdde5a5dbfa54f3
Job85 <julianjernigan@ymail.com>
2022-05-02 23:56:48 -0400

    added Character class and defined decrement_health

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 import json
+import character
+
 characters = []
 
 with open('characters.json') as json_file:
--- smash.py
+++ smash.py
@@ -1,1 +1,21 @@
 import random
+import json
+
+
+class Battle:
+    def __init__(self):
+        pass
+
+
+class Character:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.attacks = []
+
+    def decrement_health(self, attrition):
+        self.health -= attrition
+        return self.health
+
+    def attacks(self):
+        self.random.attacks
