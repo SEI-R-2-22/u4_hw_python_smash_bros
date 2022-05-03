d72f71b38907a98c520af9a7bfb7b8ec19493499
Job85 <julianjernigan@ymail.com>
2022-05-03 02:04:56 -0400

    fighter_selection doesn't print all fighters'

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ctypes.wintypes import PCHAR
 import random
 import json
 from unicodedata import name
@@ -12,7 +13,33 @@ with open('characters.json') as json_file:
     characters = json.load(json_file)
 
 
-for character in characters:
-    print(character["name"])
+# choose = input('Ready to fight? (Y/N)')
+#     if choose.lower().strip() == 'y':
 
-character_choice = input()
+def fighter_selection():
+    # fighters = []
+    for character in characters:
+        # fighters.append(character["name"])
+        print('Pick fighter!')
+        print(character["name"])
+        # print(fighters)
+        character_choice = input().strip()
+        if character_choice.lower().strip() in characters:
+            if character_choice.lower().strip() == character["name"]:
+                PC = Character(character['name'], character['attacks'])
+                print("You picked- " + PC.name)
+                break
+        else:
+            print("Invalid choice! Choose fighter from list.")
+            fighter_selection()
+
+# def choose_fighter(name):
+#     for character in characters:
+#         print(character["name"])
+
+
+def game():
+    fighter_selection()
+
+
+game()
