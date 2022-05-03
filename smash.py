b203a6285839964fe815364aac682ba41b3332cf
Piero Caceres <pierocaceres92@yahoo.com>
2022-05-02 22:45:38 -0400

    Updated smash file

--- smash.py
+++ smash.py
@@ -4,6 +4,7 @@ class Battle:
     def __init__(self, char1, char2):
         self.char1 = char1
         self.char2 = char2
+        self.start = True
 
 class Character:
     def __init__(self, name, moves):
