94e28f2b98df2d3c0edca53a5b1da369b2f85c28
Ryan Garner <rpgarner22@gmail.com>
2022-05-02 21:51:02 -0600

    in progress

--- smash.py
+++ smash.py
@@ -1,1 +1,21 @@
 import random
+
+# battle and character class
+
+class Battle:
+
+
+# need name, health, and list of their moves 
+
+class Character:
+    def __init__(self, name, health, attacks):
+        self.name = name
+        self.health = health
+        self.attacks = attacks 
+    def decrement_health(self, other):
+        self.health -= other.attacks.damage
+        print self.health
+    def attack(self, other):
+        self.attacks = random.self.attacks
+        other.health -= self.attacks.damage 
+
