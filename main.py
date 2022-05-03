27b4f1619c71871793392f29b8eab18b128ef497
Jason McCoy <jwmccoy29@gmail.com>
2022-05-03 08:09:36 -0500

    resubmit

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
 import random
 import json
+from smash import Character, Battle
+
 characters = []
 
 with open('characters.json') as json_file:
     characters = json.load(json_file)
+
+
+def obtain_character(character_name):
+    for character in characters:
+        if character_name.lower() == character["name"].lower():
+            return character
+    return None
+
+
+def game():
+    character_name = input("Type a character's name: ")
+    character = obtain_character(character_name)
+    if not character_name or character is None:
+        character = random.choice(characters)
+
+    computer = random.choice(characters)
+
+    player = Character(character["name"], character["attacks"])
+
+    competition = Character(computer["name"], computer["attacks"])
+
+    print(character["name"] + " will be facing " + computer["name"])
+
+    battle = Battle(player, competition)
+    battle.runit()
+
+
+def restart():
+    choice = input('Would you like to play again? (yes/no)')
+    if choice == 'yes':
+        game()
+    elif choice == 'no':
+        print('Thanks for playing!')
+
+    else:
+        print('That will not work. Please pick again. (yes/no)')
+        restart()
+
+
+game()
+restart()
--- smash.py
+++ smash.py
@@ -1,1 +1,34 @@
 import random
+import random
+import json
+
+character_file = open('characters.json')
+characters = json.load(character_file)
+
+
+class Character:
+    def __init__(self, name, attacks):
+        self.name = name
+        self.health = 50
+        self.moves = attacks
+
+    def attack(self, enemy):
+        attack = random.choice(self.moves)
+        enemy.health = enemy.health - attack["damage"]
+        print(self.name + " has hit " + enemy.name + " with " + attack["name"])
+        print(enemy.name + " health is now " + str(enemy.health))
+
+
+class Battle:
+    def __init__(self, player, computer):
+        self.player = player
+        self.computer = computer
+
+    def runit(self):
+        while (self.player.health > 0 and self.computer.health > 0):
+            self.player.attack(self.computer)
+            self.computer.attack(self.player)
+        if self.player.health <= 0:
+            print(self.player.name + " is the winner!")
+        elif self.computer.health <= 0:
+            print(self.computer.name + " has won!")
