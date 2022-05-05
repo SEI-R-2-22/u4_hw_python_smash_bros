8b60970ae60092c6e680331f9957f87954e97cc2
TylusDawkins <100214696+TylusDawkins@users.noreply.github.com>
2022-05-04 18:30:03 -0700

    Group 2 - Tylus Dawkins

--- smash.py
+++ smash.py
@@ -1,18 +1,19 @@
 import random
 
 class Character:
-    def __init(self,name,list):
+    def __init__(self, name, list):
         self.name = name
-        self.health = 50
+        self.health = 20
         self.attacks = list
 
-    def damage(self,hp):
+    def damage(self, hp):
         self.health -= hp
 
     def rand_attack(self):
         return random.choice(self.attacks)
 
+
 class Battle:
-    def __init__(self,player_one,player_two):
+    def __init__(self, player_one, player_two):
         self.player = player_one
         self.npc = player_two
