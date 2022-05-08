af36346f991ef6fced757a2b90b88bfd4873bfe7
milywendland <milynicwendland@gmail.com>
2022-05-08 12:18:23 -0600

    I had to do it a little differently

--- main.py
+++ main.py
@@ -1,6 +1,91 @@
 import random
-import json
-characters = []
+from smash import Battle, Character
 
-with open('characters.json') as json_file:
-    characters = json.load(json_file)
+CHARACTER_ROSTER = [
+    Character('Pikachu', [
+        {'name': 'thunderbolt', 'damage': 10},
+        {'name': 'tail whip', 'damage': 3},
+        {'name': 'tackle', 'damage': 1},
+    ]),
+    Character('Link', [
+        {'name': 'jab', 'damage': 1},
+        {'name': 'gale boomerang', 'damage': 10},
+        {'name': 'jab3', 'damage': 3},
+    ]),
+    Character('Wario', [
+        {'name': 'dash attack', 'damage': 7},
+        {'name': 'fsmash', 'damage': 10},
+        {'name': 'bair', 'damage': 12},
+    ]),
+    Character('Metaknight', [
+        {'name': 'fsmash', 'damage': 17},
+        {'name': 'ftilt', 'damage': 3},
+        {'name': 'drill rush', 'damage': 1},
+    ]),
+    Character('Lucas', [
+        {'name': 'pk fire', 'damage': 3},
+        {'name': 'uair', 'damage': 13},
+        {'name': 'psi magnet', 'damage': 8},
+    ]),
+    Character('Kirby', [
+        {'name': 'fsmash', 'damage': 15},
+        {'name': 'inhale', 'damage': 1},
+        {'name': 'uthrow', 'damage': 10},
+    ]),
+    Character('Falco', [
+        {'name': 'rapid jab finisher', 'damage': 3},
+        {'name': 'fsmash', 'damage': 15},
+        {'name': 'uair', 'damage': 10},
+    ]),
+    Character('Diddykong', [
+        {'name': 'utilt', 'damage': 6},
+        {'name': 'banana peel', 'damage': 3},
+        {'name': 'bthrow', 'damage': 10},
+    ]),
+    Character('CaptainFalcon', [
+        {'name': 'utilt', 'damage': 6},
+        {'name': 'rapid jab finisher', 'damage': 2},
+        {'name': 'falcon punch', 'damage': 25},
+    ]),
+]
+
+
+def player_character_selection():
+    print("|  CHOOSE YOUR FIGHTER!  |")
+    for index, character in enumerate(CHARACTER_ROSTER):
+        print(f'{index + 1}. {character.name}')
+
+    selection = input("Enter your selection here: ").strip()
+
+    if selection.isdigit() and 0 < int(selection) <= len(CHARACTER_ROSTER):
+        character = CHARACTER_ROSTER[int(selection) - 1]
+        print(f'Congrats, you chose {character.name}!')
+
+    else:
+        character = random_character_selection()
+        print(
+            f' \033[91m{selection}\033[0m is not valid, so we chose {character.name} for you!')
+    return character
+
+
+def computer_character_selection():
+    character = random_character_selection()
+    print(f'You will be facing off against {character.name}')
+    return character
+
+
+def random_character_selection():
+    return random.choice(CHARACTER_ROSTER)
+
+
+def game():
+    print("Welcome to the SUPER SMASH BROS tournament!")
+    player_character = player_character_selection()
+    computer_character = computer_character_selection()
+    victor = Battle([player_character, computer_character]).start_battle()
+    print(
+        f'What a show tonight. It was down to the wire, but {victor.name} was victorious! Thanks for playing!')
+
+
+if __name__ == '__main__':
+    game()
--- smash.py
+++ smash.py
@@ -1,1 +1,36 @@
 import random
+
+DEFAULT_CHARACTER_HEALTH = 50
+
+
+class Battle(object):
+    def __init__(self, battlers):
+        self.battlers = battlers
+        random.shuffle(self.battlers)
+        print(f'{self.battlers[0].name} goes first!')
+
+    def start_battle(self):
+        while True:
+            self.battlers[0].attack(self.battlers[1])
+            if self.battlers[1].health <= 0:
+                return self.battlers[0]
+            self.battlers[1].attack(self.battlers[0])
+            if self.battlers[0].health <= 0:
+                return self.battlers[1]
+
+
+class Character(object):
+    def __init__(self, name, attack_list, max_health=DEFAULT_CHARACTER_HEALTH):
+        self.name = f'\033[94m{name}\033[0m'
+        self.attack_list = attack_list
+        self.max_health = self.health = max_health
+
+    def take_damage(self, damage):
+        self.health = max(0, self.health - damage)
+        print(f'{self.name} has \033[96m{self.health}\033[0m remaining!')
+
+    def attack(self, opponent):
+        attack = random.choice(self.attack_list)
+        print(
+            f'{self.name} \033[92m{attack["name"]}\033[0m for \033[93m{attack["damage"]}\033[0m! This could be serious!')
+        opponent.take_damage(attack['damage'])
