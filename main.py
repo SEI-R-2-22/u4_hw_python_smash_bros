4ccd1d8a4c52ab23f0906840d629e869d330d9a2
Alissa Wiley <alissa.estelle.w@gmail.com>
2022-05-09 08:50:05 -0400

    Smash Bros HW Attempt

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import random
 import json
+from smash import Character, Battle
+
 characters = []
 
 with open('characters.json') as json_file:
     characters = json.load(json_file)
+
+
+player_choice = input('Choose your character: ')
+
+for character in characters:
+    if player_choice.lower() == character["name"].lower():
+        player_character = Character(character["name"], character["attacks"])
+
+comp_choice = random.choice(characters)
+comp_character = Character(comp_choice["name"], comp_choice["attacks"])
+
+
+def dual():
+    battle = Battle(player_character, comp_character)
+    while battle.character1.health > 0 and battle.character2.health > 0:
+        comp_character.decrement_health(
+            player_character.random_attack()["damage"])
+        player_character.decrement_health(
+            comp_character.random_attack()["damage"]
+        )
--- smash.py
+++ smash.py
@@ -1,1 +1,21 @@
 import random
+
+
+class Character:
+    def __init__(self, name, attacks):
+        self.name = name
+        self.health = 100
+        self.moves = attacks
+
+    def decrement_health(self, damage):
+        self.health = self.health - damage
+
+    def random_attack(self):
+        random_move = random.choice(self.moves)
+        return random_move
+
+
+class Battle:
+    def __init__(self, character1, character2):
+        self.character1 = character1
+        self.character2 = character2
